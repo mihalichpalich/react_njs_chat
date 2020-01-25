5361fd1a8cc884d5935d95384e64b4325ea56dc7
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-25 16:23:21 +0300

    сверстал страницы регистрации

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Button as BaseButton} from 'antd'
+import classNames from 'classnames'
 
 import './Button.scss'
 
-const Button = ({children}) => {
-    return <BaseButton>{children}</BaseButton>
-}
+const Button = (props) => {
+    return <BaseButton {...props} className={classNames('button', props.className, {
+        "button--large": props.size === "large"
+    })} />
+};
+
+Button.propTypes = {
+    className: PropTypes.string
+};
 
 export default Button
---
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {Form, Icon, Input} from "antd";
+import {Block, Button} from "../../../components";
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="auth__top">
+                    <h2>Войти в аккаунт</h2>
+                    <p>Пожалуйста, войдите в свой аккаунт</p>
+                </div>
+                <Block>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                placeholder="Username"
+                                size="large"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="Password"
+                                size="large"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" size="large">
+                                Войти в аккаунт
+                            </Button>
+                        </Form.Item>
+
+                        <Link className="auth__register-link" to="/register">Зарегистрироваться</Link>
+                    </Form>
+                </Block>
+            </div>
+        )
+    }
+}
+
+export default LoginForm
