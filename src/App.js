c3bb0f5dc608e1ce352cbd058c90c51f40647b6d
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-25 23:30:11 +0300

    добавил все файлы в репоиторий

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+
+import {Auth, Home} from './pages';
+import {LoginForm} from "./modules";
+
+function App() {
+  return (
+    <div className="wrapper">
+        <Route exact path={["/", "/login"]} component={Auth}/>
+        <Route exact path="/im" component={Home}/>
+    </div>
+  );
+}
+
+export default App;
