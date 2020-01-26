3c3703942bfcd317c0b1ef706a49be5e54a2b399
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-26 23:25:09 +0300

    работаю с формиком

---
+++ src/utils/validate.js
@@ -0,0 +1,23 @@
+export default ({isAuth, values, errors}) => {
+    const rules = {
+        email: (value) => {
+            if (!value) {
+                errors.email = 'Введите e-mail';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+                errors.email = 'Неверный e-mail';
+            }
+        },
+        password: (value) => {
+            if (!value) {
+                errors.password = 'Введите пароль';
+            } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/i.test(value)) {
+                errors.password = isAuth ? "Неверный пароль" : 'Слишком легкий пароль';
+            }
+        }
+    };
+
+    Object.keys(values).forEach(
+        key => rules[key] && rules[key](values[key])
+    );
+}
+
